46ff216b4b88f3e3792b6a402e95e867c7bf514d
LucasAlegre <lucasnale@gmail.com>
2019-09-23 00:56:26 -0300

    features distribution plot

--- rl_visualization/app.py
+++ rl_visualization/app.py
@@ -1,15 +1,11 @@
 from flask import Flask, send_file, make_response, render_template
 import os
 from datetime import datetime
-from rl_visualization.plotting import get_correlation_matrix_as_bytes, get_breast_cancer_df, get_pair_plot_as_bytes
 
 
 def start_app(env):
 
     app = Flask(__name__)
-
-    # get data to keep it in memory, usually you will serve this from a database or bucket, or whereever your db is sitting
-    breast_cancer_df, features_names = get_breast_cancer_df()
 
     BASE_URL = "http://localhost:5000/"
 
@@ -17,28 +13,26 @@ def start_app(env):
     def index():
         return render_template('index.html', base_url=BASE_URL)
 
-    @app.route('/plots/breast_cancer_data/pairplot/features/<features>', methods=['GET'])
-    def pairplot(features):
-        try:
-            # parse columns
-            parsed_features = [feature.strip() for feature in features.split(',')]
-            bytes_obj = get_pair_plot_as_bytes(breast_cancer_df, parsed_features)
+    @app.route('/plots/featuresdistribution', methods=['GET'])
+    def featuresdistribution():
+        bytes_obj = env.get_featuresdistribution()
 
-            return send_file(bytes_obj,
-                            attachment_filename='plot.png',
-                            mimetype='image/png')
-        except ValueError:
-            # something went wrong to return bad request
-            return make_response('Unsupported request, probably feature names are wrong', 400)
+        return send_file(bytes_obj,
+                        attachment_filename=str(datetime.now()).split('.')[0] + '_featuresdistribution.png',
+                        mimetype='image/png')
 
 
     @app.route('/plots/q-table', methods=['GET'])
-    def correlation_matrix():
-        bytes_obj = env.get_qtable_png()
+    def q_table():
+        try:
+            bytes_obj = env.get_qtable_png()
 
-        return send_file(bytes_obj,
-                        attachment_filename=str(datetime.now()).split('.')[0] + '_plot.png',
-                        mimetype='image/png')
+            return send_file(bytes_obj,
+                            attachment_filename=str(datetime.now()).split('.')[0] + '_qtable.png',
+                            mimetype='image/png')
+        except ValueError:
+            return make_response('Unsupported request, probably feature names are wrong', 400)
+
 
     app.jinja_env.auto_reload = True
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
--- rl_visualization/visualization_env.py
+++ rl_visualization/visualization_env.py
@@ -4,12 +4,14 @@ from datetime import datetime
 import gym
 import numpy as np
 import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import seaborn as sns
 from threading import Thread
 from rl_visualization.app import start_app
 
-sns.set(style='darkgrid')
+sns.set(style='whitegrid')
 sns.set_context('talk')
 
 
@@ -23,6 +25,7 @@ class VisualizationEnv(gym.Wrapper):
         """
         super().__init__(env)
         self.env = env
+        
         self.agent = agent
 
         if not os.path.exists(path):
@@ -39,13 +42,9 @@ class VisualizationEnv(gym.Wrapper):
 
     def step(self, action):
         next_obs, reward, done, info = self.env.step(action)
-        
-        if hasattr(self.env, 'discrete_observation_space'):
-            exp = {'obs': self.env.encode(self.obs)}
-            exp.update({'next_obs': self.env.encode(next_obs)})
-            exp.update({'action': action, 'reward': reward, 'done': done})
-            self.experiences.append(exp)
-            self.obs = next_obs
+
+        self.experiences.append((self.obs, action, reward, done))
+        self.obs = next_obs
 
         return next_obs, reward, done, info
 
@@ -57,13 +56,35 @@ class VisualizationEnv(gym.Wrapper):
         self.app_process = Thread(target=start_app, args=(self,))
         self.app_process.start()
 
+    def get_featuresdistribution(self):
+        f, ax = plt.subplots(figsize=(10, 10))
+        plt.title('Features Distribution')
+
+        dim = len(self.experiences[0][0])
+
+        d = []
+        for exp in self.experiences[-1000:]:
+            s = exp[0]
+            d.append({'f'+str(i): s[i] for i in range(dim)})
+        df = pd.DataFrame(d)
+
+        for i in range(dim):
+            plt.subplot(3, 4, i+1)
+            sns.distplot(df['f'+str(i)], hist=True, color="b", kde_kws={"shade": True}).set(xlim=(0, 1))
+        plt.tight_layout()
+
+        bytes_image = io.BytesIO()
+        plt.savefig(bytes_image, format='png')
+        bytes_image.seek(0)
+        return bytes_image
+
     def get_qtable_png(self):
         if self.agent is not None and hasattr(self.agent, 'q_table') and len(self.experiences) > 0:
-            plt.figure(figsize=(30,20))
+            f, ax = plt.subplots(figsize=(14, 8))
             plt.title('Q-table')
 
             df = self.q_table_to_df(num_rows=20)
-            sns.heatmap(df, annot=True, fmt="g", cmap="PiYG", linewidths=.5)
+            sns.heatmap(df, annot=True, fmt="g", cmap="PiYG", linewidths=.5, center=0.0)
             plt.tight_layout()
 
             bytes_image = io.BytesIO()
@@ -71,16 +92,16 @@ class VisualizationEnv(gym.Wrapper):
             bytes_image.seek(0)
             return bytes_image
 
-    def join(self):
-        self.app_process.join()
-
     def q_table_to_df(self, num_rows=20):
         df = []
         for exp in self.experiences[-num_rows:]:
-            s = exp['obs']
+            s = self.env.encode(exp[0])
             for i, q in enumerate(self.agent.q_table[s]):
                 df.append({'state': str(self.env.radix_decode(s)), 'action': i, 'q': q})
         df = pd.DataFrame(df)
         df.drop_duplicates(subset=None, keep='first', inplace=True)
         df = df.pivot(index='state', columns='action', values='q')
         return df
+    
+    def join(self):
+        self.app_process.join()
